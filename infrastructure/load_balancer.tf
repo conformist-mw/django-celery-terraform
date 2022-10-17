0bdfe6fd8218fae7771c8710a75e8c24a00f4bf8
Oleg Smedyuk <oleg.smedyuk@gmail.com>
2022-10-17 12:09:00 +0300

    use redirect for health check

--- infrastructure/load_balancer.tf
+++ infrastructure/load_balancer.tf
@@ -1,64 +1,64 @@
 resource "aws_lb" "production" {
-  name               = "production"
+  name = "production"
   load_balancer_type = "application"
-  internal           = false
-  security_groups    = [aws_security_group.production_lb.id]
-  subnets            = [aws_subnet.production_public_1.id, aws_subnet.production_public_2.id]
+  internal = false
+  security_groups = [aws_security_group.production_lb.id]
+  subnets = [aws_subnet.production_public_1.id, aws_subnet.production_public_2.id]
 }
 
 resource "aws_lb_target_group" "production_backend" {
-  name        = "production-backend"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.production.id
+  name = "production-backend"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.production.id
   target_type = "ip"
 
   health_check {
-    path                = "/"
-    port                = "traffic-port"
-    healthy_threshold   = 5
+    path = "/admin/"
+    port = "traffic-port"
+    healthy_threshold = 5
     unhealthy_threshold = 2
-    timeout             = 2
-    interval            = 5
-    matcher             = "200"
+    timeout = 2
+    interval = 5
+    matcher = "302"
   }
 }
 
 resource "aws_lb_listener" "production_http" {
   load_balancer_arn = aws_lb.production.id
-  port              = "80"
-  protocol          = "HTTP"
-  depends_on        = [aws_lb_target_group.production_backend]
+  port = "80"
+  protocol = "HTTP"
+  depends_on = [aws_lb_target_group.production_backend]
 
   default_action {
-    type             = "forward"
+    type = "forward"
     target_group_arn = aws_lb_target_group.production_backend.arn
   }
 }
 
 resource "aws_security_group" "production_lb" {
-  name        = "production-lb"
+  name = "production-lb"
   description = "Controls access to the ALB"
-  vpc_id      = aws_vpc.production.id
+  vpc_id = aws_vpc.production.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
